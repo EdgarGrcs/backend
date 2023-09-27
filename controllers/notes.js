3401a95b49e2c2c3694b10c42c9ffc1687a8c23f
EdgarGrcs <edgar.gricius2@gmail.com>
2023-09-27 22:09:58 +0200

    refactor

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,64 +1,58 @@
-const notesRouter = require('express').Router()
-const Note = require('../models/note')
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
 
 //What is Router():
 // isolated instance of middleware and routes
 //like a mini application, capable only of performing middleware and routing functions
 
-notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
+});
+
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.get('/:id',(request, response, next) => {
-    Note.findById(request.params.id)
-    .then(note => {
-        if (note) {
-            response.json(note)
-        } else {
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.post('/', (request, response, next) => {
-    const body = request.body
-  
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
+/**
+ * Try and catch block are not needed anymore, because the dependency "express-async-errors" does the error handling under the hood
+ * if an exception occurs, execution is automatically passed to error handling middleware
+ */
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+notesRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
     })
-  
-    note.save()
-      .then(savedNote => {
-        response.json(savedNote)
-      })
-      .catch(error => next(error))
-  })
+    .catch((error) => next(error));
+});
 
-  notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-      .then(() => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
-
-  notesRouter.put('/:id', (request, response, next) => {
-    const body = request.body
-  
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
-  
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-      .then(updatedNote => {
-        response.json(updatedNote)
-      })
-      .catch(error => next(error))
-  })
-  
-  module.exports = notesRouter
+module.exports = notesRouter;
--- utils/logger.js
+++ utils/logger.js
@@ -2,13 +2,16 @@
 // like graylog or papertrail, you would only need to make changes in one place
 
 const info = (...params) => {
+  if (process.env.NODE_ENV !== "test") 
     console.log(...params);
-}
+};
 
 const error = (...params) => {
+  if (process.env.NODE_ENV !== "test") 
     console.error(...params);
-}
+};
 
 module.exports = {
-    info,error
-}
+  info,
+  error,
+};
